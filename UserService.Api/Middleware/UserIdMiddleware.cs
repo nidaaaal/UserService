6ba7cb7caeafe789fd10e2844057f76d9fe3f04f
nidal <nidalcheruvadi@gmail.com>
2025-09-08 12:58:56 +0530

    Add project files.

---
+++ UserService.Api/Middleware/UserIdMiddleware.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Http;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace Emocare.API.Middlewares
+{
+    // You may need to install the Microsoft.AspNetCore.Http.Abstractions package into your project
+    public class UserIdMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public UserIdMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            if (httpContext.User.Identity?.IsAuthenticated == true)
+            {
+                var userIdClaim = httpContext.User.FindFirst(ClaimTypes.NameIdentifier);
+                if(userIdClaim!=null && Guid.TryParse(userIdClaim.Value,out Guid userId)){
+                    Console.Write(userId);
+                    httpContext.Items["userId"] = userId;
+                }
+            }
+           await _next(httpContext);
+        }
+    }
+
+    public static class UserIdMiddlewareExtensions
+    {
+        public static IApplicationBuilder UseUserIdMiddleware(this IApplicationBuilder builder)
+        {
+            return builder.UseMiddleware<UserIdMiddleware>();
+        }
+    }
+}
